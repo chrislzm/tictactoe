d2d74a93248ccee3b3b1d6ae100c40256f713aa0
chrislzm <me@chrisleung.com>
2018-05-06 23:49:29 -0700

    fix: Replaced function declaration with function expression for privacy
    refactor: Reorganized code

--- game.js
+++ game.js
@@ -1,13 +1,7 @@
 const TicTacToe = (function () {
-  const game = {};
 
-  // private variables
-  let currentPlayer = 'x';
-  let ownership = {
-    x: [],
-    o: []
-  };
-  const winningSets = [
+  // constants
+  const WINNING_SETS = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -18,52 +12,15 @@ const TicTacToe = (function () {
     [3, 5, 7]
   ];
 
+  // private variables
+  let currentPlayer = 'x';
+  let ownership = {
+    x: [],
+    o: []
+  };
+
   // private methods
-  function endGameIfOver() {
-    if (hasCurrentPlayerWon()) {
-      endWithMessage('Player ' + currentPlayer + ' has won!');
-    } else if (hasNoWinner()) {
-      endWithMessage('It\'s a draw!');
-    } else {
-      toggleCurrentPlayer();
-    }
-  }
-
-  function hasNoWinner() {
-    return ownership['x'].length + ownership['o'].length === 9;
-  }
-
-  function endWithMessage(msg) {
-    alert(msg);
-
-    const cells = document.getElementsByClassName('cell');
-
-    for (let i = 0, len = cells.length; i < len; i++) {
-      cells[i].classList.remove('x','o','visited');
-    }
-    ownership.x = [];
-    ownership.o = [];
-  }
-
-  function hasCurrentPlayerWon() {
-    const ownedCells = ownership[currentPlayer];
-
-    if (ownedCells.length < 3) {
-      return false;
-    }
-
-    return winningSets.filter(function (set) {
-        return set.filter(function (num) {
-            return ownedCells.indexOf(num) >= 0;
-          }).length >= 3;
-      }).length > 0;
-  }
-
-  function toggleCurrentPlayer() {
-    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
-  }
-
-  function addCellListeners() {
+  const addCellListeners = function () {
     const board = document.getElementById('board');
     board.addEventListener('click', function (e) {
       const cell = e.target;
@@ -75,12 +32,57 @@ const TicTacToe = (function () {
     });
   }
 
+  const endGameIfOver = function () {
+    if (hasCurrentPlayerWon()) {
+      endWithMessage('Player ' + currentPlayer + ' has won!');
+    } else if (hasNoWinner()) {
+      endWithMessage('It\'s a draw!');
+    } else {
+      toggleCurrentPlayer();
+    }
+  }
+
+  const hasNoWinner = function () {
+    return ownership['x'].length + ownership['o'].length === 9;
+  }
+
+  const endWithMessage = function (msg) {
+    alert(msg);
+
+    const cells = document.getElementsByClassName('cell');
+
+    for (let i = 0, len = cells.length; i < len; i++) {
+      cells[i].classList.remove('x','o','visited');
+    }
+    ownership.x = [];
+    ownership.o = [];
+  }
+
+  const hasCurrentPlayerWon = function () {
+    const ownedCells = ownership[currentPlayer];
+
+    if (ownedCells.length < 3) {
+      return false;
+    }
+
+    return WINNING_SETS.filter(function (set) {
+        return set.filter(function (num) {
+            return ownedCells.indexOf(num) >= 0;
+          }).length >= 3;
+      }).length > 0;
+  }
+
+  const toggleCurrentPlayer = function () {
+    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+  }
+
   // public methods
+  const game = {};
   game.start = function () {
     addCellListeners();
   };
-
   return game;
+
 })();
 
 document.addEventListener('DOMContentLoaded', TicTacToe.start)
